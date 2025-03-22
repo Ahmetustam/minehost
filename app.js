e7986319a41da6ad7e8d942e202b2f382c0ffed3
Ahmetustam <hgfnnhbvmj@gmail.com>
2025-03-22 16:00:42 +0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// İletişim formu için basit validasyon
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-  event.preventDefault();
-  alert("Mesajınız gönderildi! Sizinle en kısa sürede iletişime geçeceğiz.");
-  document.getElementById("contact-form").reset();
-});
